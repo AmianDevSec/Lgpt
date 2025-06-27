f8c775bee45d24589f29ab737cfe480819f7b01b
AmianDevSec <amiandevsec@gmail.com>
2025-06-27 23:51:20 +0000

    update feature fixed

--- lgpt_updater.py
+++ lgpt_updater.py
@@ -13,29 +13,25 @@ def lgpt_updater() -> None:
     output = ""
 
     try:
-        loading_thread.start()
         get_os_arch = sub.run('uname -m', shell=True, capture_output=True, check=True)
         os_arch = get_os_arch.stdout.decode().strip()
-        url = f"https://github.com/AmianDevSec/Lgpt/releases/latest/download/lgpt-{os_arch}"
+        arch_name = "amd64" if os_arch == "x86_64" else "arm64"
+
+        url = f"https://github.com/AmianDevSec/Lgpt/releases/latest/download/lgpt-{arch_name}"
 
         command = (
-            "echo 'Lgpt Updating Started...' && "
-            "[ -e /tmp/lgpt ] && rm /tmp/lgpt && "
             f"curl -SL --progress-bar {url} -o /tmp/lgpt && "
-            "echo 'Updating Lgpt...' && "
             "sudo mv /tmp/lgpt /usr/local/bin/ && "
             "sudo chmod +x /usr/local/bin/lgpt"
         )
 
         run_update= sub.run(command, shell=True, capture_output=True, check=True)
+        output = f"{GREEN}{BOLD}Update successfully.{RESET}" 
 
-        if run_update.stderr :
-            output = error_string_styled("Update failed, please try again.")
-        else:
-            output = f"{GREEN}{BOLD}Update successfully.{RESET}"
-            
     except Exception as e :
         output = error_string_styled(f"An error occurred during the update: {e}")
+    except sub.CalledProcessError as e:
+        output = error_string_styled(f"Update failed, please try again : {e.stderr.decode()}")
     finally:
         stop_event.set()
         loading_thread.join()
