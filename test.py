b3fdd7d89087346338b3a3bb35ee73e819fb831e
AmianDevSec <amiandevsec@gmail.com>
2025-06-27 19:12:05 +0000

    Small error fixed again :)

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests as r
-
-url  = "https://lgpt-back-end.onrender.com/ai/deepseek?prompt=coulduhelpme?"
-
-response = r.request(url=url, timeout=5, method="get")
-
-print(response.json()["response"])
